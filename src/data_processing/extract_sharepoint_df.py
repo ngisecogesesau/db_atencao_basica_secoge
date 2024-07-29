b012d2ea2fdef6900bfe38c2e6e789466a05ff3e
gleyce-alves <gleyce.ufrpe@gmail.com>
2024-07-29 10:05:07 -0300

    fit: add optimizing functions

--- src/data_processing/extract_sharepoint_df.py
+++ src/data_processing/extract_sharepoint_df.py
@@ -9,35 +9,42 @@ import logging
 
 load_dotenv()
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-def get_file_content(relative_url):
+def authenticate_to_sharepoint(site_url, username, password):
     """
-    Get the content of a file from SharePoint.
+    Authenticate to the SharePoint site.
 
-    :param relative_url: The relative URL of the file in SharePoint
-    :return: File content as bytes
+    :param site_url: URL of the SharePoint site
+    :param username: SharePoint username
+    :param password: SharePoint password
+    :return: ClientContext if authentication is successful, None otherwise
     """
-    site_url = os.getenv('SHAREPOINT_SITE_URL')
-    username = os.getenv('SHAREPOINT_USERNAME')
-    password = os.getenv('SHAREPOINT_PASSWORD')
-
     context_auth = AuthenticationContext(site_url)
     if context_auth.acquire_token_for_user(username, password):
         ctx = ClientContext(site_url, context_auth)
         web = ctx.web
         ctx.load(web)
         ctx.execute_query()
-
-        logging.info(f"Authenticated on site: {web.properties['Title']}")
-
-        try:
-            response = File.open_binary(ctx, relative_url)
-            return BytesIO(response.content)
-        except Exception as e:
-            logging.error(f"Error getting file content: {e}")
-            return None
+        logger.info(f"Authenticated on site: {web.properties['Title']}")
+        return ctx
     else:
-        logging.error("Authentication failed")
+        logger.error("Authentication failed")
+        return None
+
+def get_file_content(ctx, relative_url):
+    """
+    Get the content of a file from SharePoint.
+
+    :param ctx: Authenticated ClientContext
+    :param relative_url: The relative URL of the file in SharePoint
+    :return: File content as bytes, None if error occurs
+    """
+    try:
+        response = File.open_binary(ctx, relative_url)
+        return BytesIO(response.content)
+    except Exception as e:
+        logger.error(f"Error getting file content: {e}")
         return None
 
 def get_file_as_dataframes(relative_url):
@@ -47,15 +54,46 @@ def get_file_as_dataframes(relative_url):
     :param relative_url: The relative URL of the file in SharePoint
     :return: A dictionary where keys are sheet names and values are DataFrames
     """
-    file_content = get_file_content(relative_url)
-    if file_content is not None:
-        try:
-            dataframes = pd.read_excel(file_content, sheet_name=None)
-            logging.info(f"Sheets available: {list(dataframes.keys())}")
-            return dataframes
-        except Exception as e:
-            logging.error(f"Error converting content to DataFrames: {e}")
-            return None
-    else:
-        logging.error("Error getting file content.")
+    site_url = os.getenv('SHAREPOINT_SITE_URL')
+    username = os.getenv('SHAREPOINT_USERNAME')
+    password = os.getenv('SHAREPOINT_PASSWORD')
+
+    ctx = authenticate_to_sharepoint(site_url, username, password)
+    if not ctx:
         return None
+    
+    file_content = get_file_content(ctx, relative_url)
+    if not file_content:
+        return None
+    
+    try:
+        dataframes = pd.read_excel(file_content, sheet_name=None)
+        logger.info(f"Sheets available: {list(dataframes.keys())}")
+        return dataframes
+    except Exception as e:
+        logger.error(f"Error converting content to DataFrames: {e}")
+        return None
+
+def log_dataframes_info(dataframes):
+    """
+    Logs information about each DataFrame in the given dictionary and returns the dictionary.
+
+    Parameters:
+    dataframes (dict): A dictionary where the keys are sheet names and the values are DataFrames.
+
+    Returns:
+    dict: The input dictionary of DataFrames.
+    """
+    if dataframes is not None:
+        logger.info("DataFrames criados com sucesso!")
+        for sheet_name, df in dataframes.items():
+            num_rows = df.shape[0]
+            logger.info(f"\nDataFrame da aba '{sheet_name}':")
+            logger.info(f"Número de linhas: {num_rows}")
+            logger.info(f"\nPrimeiras linhas do DataFrame:\n{df.head()}")
+    else:
+        logger.error("Falha ao criar os DataFrames.")
+    
+    return dataframes
+
+
--- src/data_processing/unidades.py
+++ src/data_processing/unidades.py
@@ -1,19 +1,13 @@
 import os
 import logging
-from extract_sharepoint_df import get_file_as_dataframes
+from extract_sharepoint_df import get_file_as_dataframes, log_dataframes_info
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 relative_url = "/Shared Documents/SESAU/BI_Indicadores_Estrategicos/ANALISE_PEAB_USF_29.02_DemandaBI.xlsx"
 dataframes = get_file_as_dataframes(relative_url)
+log_dataframes_info(dataframes)
 
-if dataframes is not None:
-    logger.info("DataFrames criados com sucesso!")
-    for sheet_name, df in dataframes.items():
-        num_rows = df.shape[0]
-        logger.info(f"\nDataFrame da aba '{sheet_name}':")
-        logger.info(f"Número de linhas: {num_rows}")
-        logger.info(f"\nPrimeiras linhas do DataFrame:\n{df.head()}")
-else:
-    logger.error("Falha ao criar os DataFrames.")
+
+
