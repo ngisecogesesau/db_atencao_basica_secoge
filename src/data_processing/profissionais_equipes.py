c8954d94237491117d6f64bb7f5ad587b9739433
plz09 <r.pellizzi09@gmail.com>
2024-07-29 18:32:26 -0300

    update

--- src/data_processing/profissionais_equipes.py
+++ src/data_processing/profissionais_equipes.py
@@ -22,32 +22,14 @@ def read_profissionais_equipes():
     dataframes_usf = get_file_as_dataframes(relative_url_usf)
     dataframes_equipes_cnes = get_file_as_dataframes(relative_url_equipes_cnes)
 
-    # Debug statements to check loaded DataFrames
-    print("DataFrames loaded from USF.xlsx:", dataframes_usf.keys())
-    print("DataFrames loaded from EQUIPES_CNES.xlsx:", dataframes_equipes_cnes.keys())
-
-    # Definir colunas a serem mantidas
     usf_columns = ['NOME DO SERVIDOR(A)','VINCULO PCR']  
     equipes_cnes_columns = ['SEQ_EQUIPE', 'NM_REFERENCIA']  
 
-    # Filtrar colunas desejadas antes de processar
-    if 'USF' in dataframes_usf:
-        df_servidores = dataframes_usf['USF'][usf_columns]
-    else:
-        df_servidores = None
+    df_servidores = dataframes_usf.get('USF')[usf_columns] if dataframes_usf.get('USF') is not None else None
+    df_equipes = dataframes_equipes_cnes.get('EQUIPES_CNES')[equipes_cnes_columns] if dataframes_equipes_cnes.get('EQUIPES_CNES') is not None else None
 
-    if 'EQUIPES_CNES' in dataframes_equipes_cnes:
-        df_equipes = dataframes_equipes_cnes['EQUIPES_CNES'][equipes_cnes_columns]
-    else:
-        df_equipes = None
-
-    # Processar os DataFrames para as planilhas específicas
     df_servidores = remove_espacos_e_acentos(df_servidores)
     df_equipes = remove_espacos_e_acentos(df_equipes)
-
-    # Debug statements to check processed DataFrames
-    print("Processed DataFrame for servidores:", df_servidores.head() if df_servidores is not None else "None")
-    print("Processed DataFrame for equipes:", df_equipes.head() if df_equipes is not None else "None")
 
     return {
         'servidores': df_servidores,
