c46d5e4c54977b0618f18f0b2d276a49572d736f
plz09 <r.pellizzi09@gmail.com>
2024-08-01 18:27:18 -0300

    subindo o schema agendamentos comentado para a branch dev

--- src/data_processing/agendamentos.py
+++ src/data_processing/agendamentos.py
@@ -1,15 +1,15 @@
-import os
-import sys
-import pandas as pd
+# import os
+# import sys
+# import pandas as pd
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir = os.path.dirname(os.path.dirname(current_dir))
-sys.path.append(root_dir)
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# root_dir = os.path.dirname(os.path.dirname(current_dir))
+# sys.path.append(root_dir)
 
-from src.utils.extract_sharepoint_df import get_file_as_dataframes
-from src.utils.excel_operations import remove_espacos_e_acentos
-from src.utils.add_primary_key import add_pk
+# from src.utils.extract_sharepoint_df import get_file_as_dataframes
+# from src.utils.excel_operations import remove_espacos_e_acentos
+# from src.utils.add_primary_key import add_pk
 
-def read_asu():
+# def read_asu():
 
-    relative_url_asu = ''
+#     relative_url_asu = ''
