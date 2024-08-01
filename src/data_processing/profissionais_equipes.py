8b50d9a8ab1cd6deb5aff47102133f4e531b386b
plz09 <r.pellizzi09@gmail.com>
2024-08-01 18:16:30 -0300

    adicionando tabela usf_mais e simplificando codigo

--- src/data_processing/profissionais_equipes.py
+++ src/data_processing/profissionais_equipes.py
@@ -17,31 +17,55 @@ def read_profissionais_equipes():
 
     :return: Um dicionário com DataFrames processados para 'servidores' e 'equipes'
     """
-    relative_url_usf = "/Shared Documents/SESAU/BI_Indicadores_Estrategicos/USF.xlsx"
-    relative_url_equipes_cnes = '/Shared Documents/SESAU/BI_Indicadores_Estrategicos/dados_estabelecimento_equipes_cnes_mar.xlsx'
 
-    dataframes_usf = get_file_as_dataframes(relative_url_usf)
-    dataframes_equipes_cnes = get_file_as_dataframes(relative_url_equipes_cnes)
+    # Tabela EQUIPES_CNES (nome original)
+    url_equipes_cnes = '/Shared Documents/SESAU/BI_Indicadores_Estrategicos/dados_estabelecimento_equipes_cnes_mar.xlsx'
+    df_equipes_cnes = get_file_as_dataframes(url_equipes_cnes)
+
+    # df_equipes é a nova nomenclatura apos normalizacao
+    #df_equipes = df_equipes_cnes.get('in')[equipes_cnes_columns] if df_equipes_cnes.get('in') is not None else None
+    df_equipes_cnes = df_equipes_cnes['in']
+    df_equipes_columns = ['CNES', 'SEQ_EQUIPE', 'DS_EQUIPE', 'NM_REFERENCIA', 'TURNO_ATEND', 'CRIACAO_EQUIPE', 'DT_DESATIVACAO', 
+                            'ID_TP_EQUIPE', 'TP_EQUIPE', 'SG_EQUIPE' ]
+    
+    df_equipes = df_equipes_cnes[df_equipes_columns]
+    df_equipes = remove_espacos_e_acentos(df_equipes)
+    df_equipes = add_pk(df_equipes, 'equipes')
+
+    # Tabela USF (nome original)
+    url_usf = "/Shared Documents/SESAU/BI_Indicadores_Estrategicos/USF.xlsx"
+    df_usf = get_file_as_dataframes(url_usf)
 
     usf_columns = ['NOME DO SERVIDOR(A)', 'SITUAÇAO FUNCIONAL', 'PERFIL DO CARGO',  'CODIGO UNIDADE DE LOTAÇAO', 
                    'PERFIL UNIDADE DE LOTACAO', 'DISTRITO', 'EQUIPE', 'CNES UNIDADE DE LOTAÇAO', 
                     'SETOR', 'TURNO DE TRABALHO']  
     
-    equipes_cnes_columns = ['CNES', 'SEQ_EQUIPE', 'DS_EQUIPE', 'NM_REFERENCIA', 'TURNO_ATEND', 'CRIACAO_EQUIPE', 'DT_DESATIVACAO', 
-                            'ID_TP_EQUIPE', 'TP_EQUIPE', 'SG_EQUIPE']  
-
-    df_servidores = dataframes_usf.get('USF')[usf_columns] if dataframes_usf.get('USF') is not None else None
-    df_equipes = dataframes_equipes_cnes.get('in')[equipes_cnes_columns] if dataframes_equipes_cnes.get('in') is not None else None
-
-
-
+    # df_servidores é a nova nomenclatura apos normalizacao
+    df_usf = df_usf['USF']
+    df_servidores = df_usf[usf_columns]
     df_servidores = remove_espacos_e_acentos(df_servidores)
-    df_equipes = remove_espacos_e_acentos(df_equipes)
-
     df_servidores = add_pk(df_servidores, 'servidores')
-    df_equipes = add_pk(df_equipes, 'equipes')
+
+    # Tabela USF + (nome_original)
+    url_usf_mais = "/Shared Documents/SESAU/BI_Indicadores_Estrategicos/ANALISE_PEAB_USF_29.02_DemandaBI.xlsx"
+    df_usf_mais = get_file_as_dataframes(url_usf_mais)
+
+    usf_mais_columns = ['CNES', 'DISTRITO', 'Nº DA ESF', 'TURNO DA ESF', 'HORÁRIO DA ESF', 'MÉDICO DA ESF', 'ENFERMEIRO ESF', 
+                        'TÉCNICO ESF', 'ACS', 'Nº ESB', 'TURNO ESB', 'HORÁRIO ESB', 'CIR. DENTISTA', 'ASB', 'RECEPCIONISTA', 
+                        'TURNO DO RECEPCIONISTA', 'HORÁRIO DO RECEPCIONISTA', 'REGULAÇÃO', 'TURNO DO PROF. REGULAÇÃO', 'HORÁRIO DO PROF. REGULAÇÃO', 
+                        'FARMÁCIA', 'TURNO DO PROF. FARMÁCIA', 'HORÁRIO DO PROF. FARMÁCIA']
+    
+    df_usf_mais = df_usf_mais['USF+']
+    df_usf_mais = df_usf_mais[usf_mais_columns]
+    df_usf_mais = remove_espacos_e_acentos(df_usf_mais)
+    df_usf_mais = add_pk(df_usf_mais, 'equipes_usf_mais')
+
+    print(df_equipes)
 
     return {
         'servidores': df_servidores,
-        'equipes': df_equipes
+        'equipes': df_equipes,
+        'equipes_usf_mais': df_usf_mais
     }
+
+read_profissionais_equipes()
