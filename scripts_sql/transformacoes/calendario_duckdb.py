7884f377d26615418bb0a5e8448cb2ff9c7e51d1
plz09 <r.pellizzi09@gmail.com>
2024-08-05 18:04:23 -0300

    update: refatorando criacao da tabela calendario

--- scripts_sql/transformacoes/calendario_duckdb.py
+++ scripts_sql/transformacoes/calendario_duckdb.py
@@ -1,53 +1,8 @@
 import duckdb
-import pandas as pd
-from datetime import datetime, timedelta
+from src.data_processing.calendario import create_calendario
 
-# Funções auxiliares para traduzir dia da semana e mês
-def traduzir_dia_semana(dia):
-    dias = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
-    return dias[dia.weekday()]
-
-def traduzir_mes_abreviado(mes):
-    meses_abreviados = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
-    return meses_abreviados[mes-1]
-
-def traduzir_mes_completo(mes):
-    meses_completos = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-    return meses_completos[mes-1]
-
-# Função para criar calendário
-def create_calendario_table(start_date, end_date):
-    delta = end_date - start_date
-    data = []
-
-    for i in range(delta.days + 1):
-        dia = start_date + timedelta(days=i)
-        ano = dia.year
-        mes = dia.month
-        dia_mes = dia.day
-        nome_dia = traduzir_dia_semana(dia)
-        dia_semana = dia.weekday()
-        mes_abreviado = traduzir_mes_abreviado(mes)
-        quadrimestre = (mes - 1) // 4 + 1
-        ano_quadrimestre = f"{ano}.{quadrimestre}"
-        mes_completo = traduzir_mes_completo(mes)
-        mvm = int(dia.strftime('%Y%m'))
-
-        data.append((dia, ano, mes, dia_mes, nome_dia, dia_semana, mes_abreviado, quadrimestre, ano_quadrimestre, mes_completo, mvm))
-
-    return pd.DataFrame(data, columns=[
-        'data_dma', 'ano', 'mes', 'dia', 'nome_dia', 'dia_semana', 'mes_abreviado', 'quadrimestre', 
-        'ano_quadrimestre', 'mes_completo', 'mvm'
-    ])
-
-start_date = datetime(2020, 1, 1)
-end_date = datetime(2025, 12, 31)
-df_calendario = create_calendario_table(start_date, end_date)
-# Cria primary key
-df_calendario.insert(0, 'id_calendario', range(1, 1 + len(df_calendario)))
-
-def insert_data_calendario_table(con):
-
+def create_table(con):
+    # Criando a tabela no DuckDB
     con.execute("""
     CREATE TABLE calendario (
         id_calendario INTEGER PRIMARY KEY,
@@ -65,13 +20,26 @@ def insert_data_calendario_table(con):
     )
     """)
 
-    
+def insert_data_calendario_table(con, df_calendario):
     # Inserindo dados no DuckDB
     con.append('calendario', df_calendario)
+
+def create_calendario_table(con):
+    # Criando a tabela
+    create_table(con)
+    
+    # Gerando o DataFrame de calendário
+    data_calendario = create_calendario()
+    df_calendario = data_calendario['calendario']  # Acessando o DataFrame retornado pela função create_calendario
+    
+    # Inserindo dados na tabela
+    insert_data_calendario_table(con, df_calendario)
+    
+    # Recuperando dados inseridos para retorno
     df_calendario_duckdb = con.execute("SELECT * FROM calendario").fetchdf()
     return df_calendario_duckdb
 
 if __name__ == '__main__':
     con = duckdb.connect(database=':memory:')
-    df_calendario_duckdb = insert_data_calendario_table(con)
+    df_calendario_duckdb = create_calendario_table(con)
     print(df_calendario_duckdb)
