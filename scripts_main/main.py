65a9c3676110c4596ce88e60d0bb5e4499fa9231
gleyce-alves <gleyce.ufrpe@gmail.com>
2024-08-06 14:48:18 -0300

    fix: refactoring duckdb main

--- scripts_main/main.py
+++ scripts_main/main.py
@@ -8,13 +8,8 @@ import duckdb
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.database import create_engine_to_db, create_schemas
-from scripts_sql.transformacoes.profissionais_equipes_duckdb import create_servidores_table, create_equipes_table, create_tipo_equipe_table, update_equipes_table, update_servidores_table, create_equipes_usf_mais_table, create_gerentes_table, update_gerentes_table, update_equipes_usf_mais_table
-from scripts_sql.transformacoes.unidades_duckdb import create_unidades_table, create_tipo_unidade_table, create_horarios_table, create_info_unidades_table, create_distritos_table, update_unidades_table
-from scripts_sql.transformacoes.asu_duckdb import create_asu_classificacao_table, create_asu_monitora_table, create_equipes_asu_table, create_unidades_equipes_asu, update_asu_monitora_table, update_equipes_asu_relacionamento_equipes, update_equipes_asu_relacionamento_unidades, update_unidades_quipes_asu_relacionamento_unidades
-from scripts_sql.transformacoes.agendamentos_duckdb import create_agendamentos_table, update_agendamentos_table
-from scripts_sql.transformacoes.atendimentos_duckdb import create_atendimentos_table, update_atendimentos_table
 from src.data_processing import get_data_processing_functions
-from scripts_sql.transformacoes.calendario_duckdb import create_calendario_table
+from scripts_sql.transformacoes import execute_transformations_and_save
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
@@ -27,180 +22,25 @@ def db_connection(config):
     finally:
         engine.dispose()
 
-def process_data(engine, schemas):
-    con = duckdb.connect(database=':memory:')
-    
+def register_tables_in_duckdb(con, schemas):
     for schema_name, read_data_func in schemas.items():
         dfs = read_data_func()
-        
-        if dfs is None:
-            logger.error(f"A função {read_data_func} retornou None.")
-            continue
-        
-        # Verificar se dfs é um dicionário
-        if not isinstance(dfs, dict):
+        if dfs is None or not isinstance(dfs, dict):
             logger.error(f"A função {read_data_func} retornou um objeto não esperado: {type(dfs)}")
             continue
-        
         for table_name, df in dfs.items():
             if isinstance(df, pd.Series):
                 df = df.to_frame()
             full_table_name = f"{table_name}_temp"
-            print(f"\nDataFrame for {full_table_name}:")
-            print(df.head())
-          
             con.register(full_table_name, df)
             logger.info(f"Registered {full_table_name} in DuckDB")
+    return con.execute("SHOW TABLES").fetchall()
 
-    registered_tables = con.execute("SHOW TABLES").fetchall()
+def process_data(engine, schemas):
+    con = duckdb.connect(database=':memory:')
+    registered_tables = register_tables_in_duckdb(con, schemas)
     logger.info("Tabelas registradas no DuckDB: %s", registered_tables)
-
-    # Passo 2: Executar transformações diretamente no DuckDB
-
-    # Tabelas do schema unidades
-    df_unidades = create_unidades_table(con)
-    df_tipo_unidade = create_tipo_unidade_table(con)
-    df_horarios = create_horarios_table(con)
-    df_info_unidades = create_info_unidades_table(con)
-    df_distritos = create_distritos_table(con)
-    df_update_unidades = update_unidades_table(con)
-
-    # Tabelas schema profissionais_equipes
-    # Cria tabela servidores no DuckDB
-    df_servidores = create_servidores_table(con)
-    # Atualiza tabela servidores do DuckDB
-    df_update_servidores = update_servidores_table(con)
-    # Cria tabela equipes no DuckDB
-    df_equipes = create_equipes_table(con)
-    # Atualiza tabela equipes
-    df_update_equipes = update_equipes_table(con)
-    # Cria tabela tipo_equipe no DuckDb
-    df_tipo_equipe = create_tipo_equipe_table(con)
-    # Cria tabela equipes_usf_mais 
-    df_equipes_usf_mais = create_equipes_usf_mais_table(con)
-    # Atualiza tabela equipes_usf_mais
-    df_update_equipes_usf_mais = update_equipes_usf_mais_table(con)
-    # Cria a tabela gerente no duckdb
-    df_gerentes = create_gerentes_table(con)
-    # Atualiza tabela gerentes no duckdb
-    df_update_gerentes = update_gerentes_table(con)
-
-    # Tabelas schemas asu
-    # Cria tabela asu_monitora
-    df_asu_monitora = create_asu_monitora_table(con)
-    # Atualiza tabela asu_monitora
-    df_update_asu_monitora_table = update_asu_monitora_table(con)
-    # Cria tabela asu_classificacao
-    df_asu_classificacao = create_asu_classificacao_table(con)
-    # Cria tabela equipes_asu
-    df_equipes_asu = create_equipes_asu_table(con)
-    # Atualiza tabela equipes_asu
-    df_update_equipes_asu = update_equipes_asu_relacionamento_equipes(con)
-    df_update_equipes_asu = update_equipes_asu_relacionamento_unidades(con)
-    # Cria tabelas unidades_equipes_asu
-    df_unidades_equipes_asu = create_unidades_equipes_asu(con)
-    # Atualiza tabela unidades_equipes_asu
-    df_update_unidades_equipes_asu = update_unidades_quipes_asu_relacionamento_unidades(con)
-
-    # Tabelas do schema agendamentos
-    df_agendamentos = create_agendamentos_table(con)
-    df_update_agendamentos = update_agendamentos_table(con)
-
-    # Tabelas do schema atendimentos
-    df_atendimentos = create_atendimentos_table(con)
-    df_update_atendimentos = update_atendimentos_table(con)
-
-    # Cria tabela calendario
-    df_calendario_duckdb = create_calendario_table(con)
-
-
-    try:
-        df_unidades.to_sql('unidades', engine, schema='unidades', if_exists='replace', index=False)
-        logger.info("Tabela 'unidades' salva no esquema 'unidades' do banco de dados PostgreSQL com sucesso.")
-        
-        df_tipo_unidade.to_sql('tipoUnidade', engine, schema='unidades', if_exists='replace', index=False)
-        logger.info("Tabela 'tipoUnidade' salva no esquema 'unidades' do banco de dados PostgreSQL com sucesso.")
-
-        df_horarios.to_sql('horarios', engine, schema='unidades', if_exists='replace', index=False)
-        logger.info("Tabela 'horarios' salva no esquema 'unidades' do banco de dados PostgreSQL com sucesso.")
-        
-        df_info_unidades.to_sql('info_unidades', engine, schema='unidades', if_exists='replace', index=False)
-        logger.info("Tabela 'inf_unidades' salva no esquema 'unidades' do banco de dados PostgreSQL com sucesso.")
-
-        df_distritos.to_sql('distritos', engine, schema='unidades', if_exists='replace', index=False)
-        logger.info("Tabela 'distritos' salva no esquema 'unidades' do banco de dados PostgreSQL com sucesso.")
-        
-        df_servidores.to_sql('servidores', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'servidores' salva no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-
-        df_update_servidores.to_sql('servidores', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'serviodres' atualizada no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-
-        df_equipes.to_sql('equipes', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'equipes' salva no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-
-        df_update_equipes.to_sql('equipes', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'equipes' atualizada no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-
-        df_tipo_equipe.to_sql('tipo_equipe', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'tipo_equipe' salva no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-        
-        df_equipes_usf_mais.to_sql('equipes_usf_mais', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'equipes_usf_mais' atualizada no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-        
-        df_update_equipes_usf_mais.to_sql('equipes_usf_mais', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'equipes_usf_mais' atualizada no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-
-        df_tipo_equipe.to_sql('tipo_equipe', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'tipo_equipe' salva no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-        
-        df_gerentes.to_sql('gerentes', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'gerentes' salva no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-        
-        df_update_gerentes.to_sql('gerentes', engine, schema='profissionais_equipes', if_exists='replace', index=False)
-        logger.info("Tabela 'gerentes' atualizada no esquema 'profissionais_equipes' do banco de dados PostgreSQL com sucesso.")
-        
-        df_update_unidades.to_sql('unidades', engine, schema='unidades', if_exists='replace', index=False)
-        logger.info("Tabela 'unidades' atualizada no esquema 'unidades' do banco de dados PostgreSQL com sucesso.")
-        
-        df_asu_monitora.to_sql('asu_monitora', engine, schema='asu', if_exists='replace', index=False)
-        logger.info("Tabela 'asu_monitora' criada no esquema 'asu' do banco de dados PostgreSQL com sucesso.")
-        
-        df_update_asu_monitora_table.to_sql('asu_monitora', engine, schema='asu', if_exists='replace', index=False)
-        logger.info("Tabela 'asu_monitora' atualizada no esquema 'asu' do banco de dados PostgreSQL com sucesso.")
-
-        df_asu_classificacao.to_sql('asu_classificacao', engine, schema='asu', if_exists='replace', index=False)
-        logger.info("Tabela 'asu_classificacao' criada no esquema 'asu' do banco de dados PostgreSQL com sucesso.") 
-          
-        df_equipes_asu.to_sql('equipes_asu', engine, schema='asu', if_exists='replace', index=False)
-        logger.info("Tabela 'equipes_asu' criada no esquema 'asu' do banco de dados PostgreSQL com sucesso.") 
-        
-        df_update_equipes_asu.to_sql('equipes_asu', engine, schema='asu', if_exists='replace', index=False)
-        logger.info("Tabela 'equipes_asu' atualizada no esquema 'asu' do banco de dados PostgreSQL com sucesso.") 
-        
-        df_unidades_equipes_asu.to_sql('unidades_equipes_asu', engine, schema='asu', if_exists='replace', index=False)
-        logger.info("Tabela 'unidades_equipes_asu' criada no esquema 'asu' do banco de dados PostgreSQL com sucesso.") 
-        
-        df_update_unidades_equipes_asu.to_sql('unidades_equipes_asu', engine, schema='asu', if_exists='replace', index=False)
-        logger.info("Tabela 'unidades_equipes_asu' atualizada no esquema 'asu' do banco de dados PostgreSQL com sucesso.") 
-
-        df_agendamentos.to_sql('agendamentos', engine, schema='agendamentos', if_exists='replace', index=False)
-        logger.info("Tabela 'agendamentos' criada no esquema 'agendamentos' do banco de dados PostgreSQL com sucesso.")
-      
-        df_update_agendamentos.to_sql('agendamentos', engine, schema='agendamentos', if_exists='replace', index=False)
-        logger.info("Tabela 'agendamentos' atualizada no esquema 'agendamentos' do banco de dados PostgreSQL com sucesso.")
-
-        df_atendimentos.to_sql('atendimentos', engine, schema='atendimentos', if_exists='replace', index=False)
-        logger.info("Tabela 'atendimentos' criada no esquema 'atendimentos' do banco de dados PostgreSQL com sucesso.")
-      
-        df_update_atendimentos.to_sql('atendimentos', engine, schema='atendimentos', if_exists='replace', index=False)
-        logger.info("Tabela 'atendimentos' atualizada no esquema 'atendimentos' do banco de dados PostgreSQL com sucesso.")   
-    
-        df_calendario_duckdb.to_sql('calendario', engine, schema='calendario', if_exists='replace', index=False)
-        logger.info("Tabela 'calendario' criada no esquema 'calendario' do banco de dados PostgreSQL com sucesso.") 
-        
-    except Exception as e:
-        logger.error(f"Erro ao processar tabelas: {e}")
+    execute_transformations_and_save(con, engine)
 
 def main():
     config = {
