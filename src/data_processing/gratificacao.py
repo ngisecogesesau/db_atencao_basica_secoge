c69eba71f74c6e8b3831c2afd193ed994d3860cb
Lucas Adelino <lcsadelino@gmail.com>
2024-09-20 10:35:58 -0300

    Add support for reading from Google Sheets

--- src/data_processing/gratificacao.py
+++ src/data_processing/gratificacao.py
@@ -1,13 +1,19 @@
-# import pandas as pd 
+import pandas as pd
+import os
+import sys
 
-# from src.utils.extract_sharepoint_df import get_file_as_dataframes
-# from src.utils.excel_operations import remove_espacos_e_acentos
-# from src.utils.add_primary_key import add_pk
+# Codigo aqui por enquanto caso seja util pra debugar; remover depois
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+# root_dir = os.path.dirname(os.path.dirname(current_dir))
+## print(root_dir)
+# sys.path.append(root_dir)
 
-# def read_gratificacoes():
+from src.utils.extract_googlesheet_df import get_file_as_dataframes
+from src.utils.excel_operations import remove_espacos_e_acentos
+from src.utils.add_primary_key import add_pk
 
-#     url_gratificacoes = 'aguardar link sheets'
 
-#     return {
-#         'gratificacoes'; df_gratificacoes
-#     }
+def read_gratificacoes():
+    url_gratificacoes = "1Kp5aaAAr9o9qXqy-JRPglKwK3TJM-O3PiS6-zzI6iBA"
+    df_gratificacoes = get_file_as_dataframes(url_gratificacoes)
+    # ... resto do codigo ...
