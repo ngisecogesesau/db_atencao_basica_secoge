fd72034fa8f65419a556b058d0b5b0ad6696a20f
plz09 <r.pellizzi09@gmail.com>
2024-09-20 10:02:54 -0300

    update: comentando codigo gratificacao e aguardando update do sheets

---
+++ src/data_processing/gratificacao.py
@@ -0,0 +1,13 @@
+# import pandas as pd 
+
+# from src.utils.extract_sharepoint_df import get_file_as_dataframes
+# from src.utils.excel_operations import remove_espacos_e_acentos
+# from src.utils.add_primary_key import add_pk
+
+# def read_gratificacoes():
+
+#     url_gratificacoes = 'aguardar link sheets'
+
+#     return {
+#         'gratificacoes'; df_gratificacoes
+#     }
